02329a89011db6bf92249576418009716bbbe70a
socrateslee <socrateslee@users.noreply.github.com>
2025-03-19 22:16:44 +0800

    - fix package name for getting version; - fix dependency and bump version to 0.1.5

--- paipe/__init__.py
+++ paipe/__init__.py
@@ -2,6 +2,6 @@
 import importlib.metadata
 
 try:
-    __version__ = importlib.metadata.version("hostsed")
+    __version__ = importlib.metadata.version("paipe")
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0"

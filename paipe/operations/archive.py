cb2b31bf5960a1fe5b1bbce5df0d307b016897e9
socrateslee <socrateslee@users.noreply.github.com>
2025-04-18 21:28:40 +0800

    Update archive operation to support multiple wrapping methods(html/markdown/none)

--- paipe/operations/archive.py
+++ paipe/operations/archive.py
@@ -2,7 +2,9 @@
 Convert a list of files into a markdown of file contents.
 '''
 import os
-from typing import List, Tuple
+from typing import List, Tuple, Literal, TypeVar
+
+WrapType = Literal['html', 'markdown', 'none']
 
 
 def split_to_filelist(output):
@@ -16,22 +18,53 @@ def split_to_filelist(output):
 
 
 def wrap_html_comment(filename, content):
+    '''
+    Wrap content in html comment, aka. <!--- comment --->
+    '''
     return f'<!-- begin {filename} -->\n{content}\n<!-- end {filename} -->\n\n'
 
 
-def archive_to_markdown(files, content_list: List[Tuple[str, str]]=None):
+def wrap_markdown_comment(filename, content):
+    '''
+    Wrap content in markdown comment, aka. [comment]: #
+    '''
+    return f'[begin {filename}]: #\n{content}\n[end {filename}]: #\n\n'
+
+
+def wrap_none(__, content):
+    return content
+
+
+def get_wrap_method(wrap: WrapType):
+    if wrap == 'markdown':
+        return wrap_markdown_comment
+    elif wrap == 'none':
+        return wrap_none
+    else:
+        return wrap_html_comment
+
+
+def archive_to_markdown(files,
+                        content_list: List[Tuple[str, str]],
+                        wrap_method=None
+    ):
+    wrap_method = wrap_method or wrap_none
     markdown = ''
     for filename in files:
         if os.path.isfile(filename):
             with open(filename, 'r') as fd:
-                markdown += wrap_html_comment(filename, fd.read())
+                markdown += wrap_method(filename, fd.read())
     if content_list:
         for (filename, content) in content_list:
-            markdown += wrap_html_comment(filename, content)
+            markdown += wrap_method(filename, content)
     return markdown
 
 
-def archive(output, filelist: list | None=None, use_stdin_as: str='list'):
+def archive(output,
+            filelist: list | None=None,
+            use_stdin_as: str='list',
+            wrap: Literal['html', 'markdown', 'none']=None
+    ):
     '''
     Convert the filelist to content markdown.
     '''
@@ -43,4 +76,5 @@ def archive(output, filelist: list | None=None, use_stdin_as: str='list'):
         filelist.extend(pipe_filelist)
     else:
         content_list.append(('STDIN', output))
-    return archive_to_markdown(filelist)
+    wrap_method = get_wrap_method(wrap)
+    return archive_to_markdown(filelist, content_list, wrap_method=wrap_method)
--- paipe/operations/subcli.py
+++ paipe/operations/subcli.py
@@ -12,6 +12,14 @@ def build_command_parser(commond_parser: argparse.ArgumentParser):
         type=str, default='list', choices=['list', 'content'],
         help='Use stdin as list of filenames to archive, or simply as content to archive')
     cmd_archive.add_argument(
+        '--wrap',
+        type=str, default='html', choices=['html', 'markdown', 'none'],
+        help='''The method to wrap the content of each file(and mark the filename)
+html: use <!-- -->
+markdown: use []: #
+none: do nothing
+''')
+    cmd_archive.add_argument(
         'filelist',
         type=str, nargs='*', metavar='FILENAME',
         help='Lisf of filenames to archvie')
